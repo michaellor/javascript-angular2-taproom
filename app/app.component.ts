f295652f08a95edfd158056cbbd0936f2b01c8b9
michael lor and polina nenchev <michael.vang.1985@gmail.com>
2016-03-23 14:14:02 -0700

    Add brand,price and alcohol to display

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,43 +1,44 @@
 import { Component, EventEmitter } from 'angular2/core';
-//import { KegListComponent } from './keg-list.component';
+import { KegListComponent } from './keg-list.component';
+import { Keg } from './keg.model';
 
-@Component({
-  selector: 'keg-display',
-  inputs: ['keg'],
-  template: `
-    <h3>"hello world"</h3>
-  `
-})
-export class KegComponent {
-  public keg: Keg;
-}
+// @Component({
+//   selector: 'keg-display',
+//   inputs: ['keg'],
+//   template: `
+//     <h3>"hello world"</h3>
+//   `
+// })
+// export class KegComponent {
+//   public keg: Keg;
+// }
 
-@Component({
-  selector: 'keg-list',
-  inputs: ['kegList'],
-  outputs: ['onKegSelect'],
-  //directives: [KegComponent],
-  template: `
-    <keg-display *ngFor="#currentKeg of kegList"
-      (click)="kegClicked(currentKeg)"
-      [class.selected]="currentKeg === selectedKeg"
-      [keg]="currentKeg">
-    </keg-display>
-  `
-})
-export class KegListComponent {
-  public kegList: Keg[];
-  public onKegSelect: EventEmitter<Keg>;
-  public selectedKeg: Keg;
-  constructor() {
-    this.onKegSelect = new EventEmitter();
-  }
-  kegClicked(clickedKeg: Keg): void {
-    console.log('child', clickedKeg);
-    this.selectedKeg = clickedKeg;
-    this.onKegSelect.emit(clickedKeg);
-  }
-}
+// @Component({
+//   selector: 'keg-list',
+//   inputs: ['kegList'],
+//   outputs: ['onKegSelect'],
+//   //directives: [KegComponent],
+//   template: `
+//     <keg-display *ngFor="#currentKeg of kegList"
+//       (click)="kegClicked(currentKeg)"
+//       [class.selected]="currentKeg === selectedKeg"
+//       [keg]="currentKeg">
+//     </keg-display>
+//   `
+// })
+// export class KegListComponent {
+//   public kegList: Keg[];
+//   public onKegSelect: EventEmitter<Keg>;
+//   public selectedKeg: Keg;
+//   constructor() {
+//     this.onKegSelect = new EventEmitter();
+//   }
+//   kegClicked(clickedKeg: Keg): void {
+//     console.log('child', clickedKeg);
+//     this.selectedKeg = clickedKeg;
+//     this.onKegSelect.emit(clickedKeg);
+//   }
+// }
 
 @Component({
   selector: 'my-app',
@@ -45,7 +46,7 @@ export class KegListComponent {
   template: `
     <div class="container">
       <h1>Tap Room</h1>
-      <keg-display *ngFor="#keg of kegs"><li>{{ keg.name }}</li></keg-display>
+      <keg-display *ngFor="#keg of kegs"><li>{{ keg.name }},{{ keg.brand }}, {{ keg.price}}, {{ keg.alcohol}}</li></keg-display>
     </div>
   `
 })
@@ -62,9 +63,11 @@ export class AppComponent {
   }
 }
 
-export class Keg {
-  public empty: boolean = false;
-  constructor(public name: string, public brand: string, public price: number, public alcohol: string, public id: number) {
+// this gets its own file only for the parent in file called something.model.ts
 
-  }
-}
+// export class Keg {
+//   public empty: boolean = false;
+//   constructor(public name: string, public brand: string, public price: number, public alcohol: string, public id: number) {
+//
+//   }
+// }
--- app/keg.component.ts
+++ app/keg.component.ts
@@ -1,1 +1,13 @@
 import { Component } from 'angular2/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'keg-display',
+  inputs: ['keg'],
+  template: `
+    <h3>"hello world"</h3>
+  `
+})
+export class KegComponent {
+  public keg: Keg;
+}
