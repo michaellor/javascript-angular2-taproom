391d66c173d1d570905bff11bf31bcedb02c979b
michael lor and polina nenchev <polina.mihova@yahoo.com>
2016-03-23 16:11:22 -0700

    Add some notes in the keg.model and keg.component file for future development

--- app/keg.model.ts
+++ app/keg.model.ts
@@ -1,4 +1,8 @@
 export class Keg {
+  // create a public class of number for pours
+  // public pours: number = 124;
+
+  // if that is created create a file that is for pour 
   public empty: boolean = false;
   constructor(public name: string, public brand: string, public price: number, public alcohol: string, public id: number) {
 
