04fdc59497f8ac5615724a11e1dd9c7a4013abbc
michael lor and polina nenchev <polina.mihova@yahoo.com>
2016-03-24 14:03:02 -0700

    add color filter based on beer quantity in keg

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -8,12 +8,15 @@ import { PourComponent } from './pour.component';
   directives: [PourComponent],
   template: `
     <div class="container">
-      <div>
-        <h3>Name: {{ keg.name }}</h3>
-        <h4>Brand: {{ keg.brand }}</h4>
-        <p>Price: $ {{ keg.price }}</p>
-        <p>Alochol Content: {{ keg.alcohol }} %</p>
-        <p>Amount of alcohol left: {{ keg.pours }}
+      <div
+        [class.pink]="keg.pours <= 124"
+        [class.green]="keg.pours <=110"
+        [class.red]="keg.pours === 0">
+          <h3>Name: {{ keg.name }}</h3>
+          <h4>Brand: {{ keg.brand }}</h4>
+          <p>Price: $ {{ keg.price }}</p>
+          <p>Alochol Content: {{ keg.alcohol }} %</p>
+          <p>Amount of alcohol left: {{ keg.pours }}
       </div>
       <keg-pour *ngIf="keg.pours > 0" [pourKeg]="keg"></keg-pour>
 
